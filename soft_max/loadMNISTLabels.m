35ba293280febe44de21b7310f7d6add225ca213
aSmallsheep <hsdlxf2010@126.com>
2016-09-04 11:52:25 +0800

    2st

--- soft_max/loadMNISTLabels.m
+++ soft_max/loadMNISTLabels.m
@@ -1,6 +1,4 @@
 function labels = loadMNISTLabels(filename)
-%loadMNISTLabels returns a [number of MNIST images]x1 matrix containing
-%the labels for the MNIST images
 
 fp = fopen(filename, 'rb');
 assert(fp ~= -1, ['Could not open ', filename, '']);
